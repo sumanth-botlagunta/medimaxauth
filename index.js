13f537c31beb43d6e497560069779d7aebe94ba6
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-03-13 23:11:32 +0530

    index

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require("express");
-var cors = require("cors");
 var app = express();
 var db = require("./db");
 
 const port = process.env.PORT || 8000;
-
+var cors = require("cors");
 app.use(cors());
 
 const Authcontroller = require("./authentication/authcontroller");
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello welcome to authapi</h1>");
+  });
 
 app.use('api/authentication', Authcontroller);
 
