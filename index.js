1d333992a49537f706c3f12bc838386fce038fa0
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-03-13 17:19:03 +0530

    auth final

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+
+const Authcontroller = require("./authentication/authcontroller");
+
+app.use('api/auth', Authcontroller);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
