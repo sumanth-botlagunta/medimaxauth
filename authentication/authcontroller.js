ebbbdb07bfb9efb0529d811d3c4e2ec22fccebec
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-03-13 22:28:13 +0530

    auth code

--- authentication/authcontroller.js
+++ authentication/authcontroller.js
@@ -9,36 +9,29 @@ var bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-router.get("/users", function (req, res) {
+router.get('/users',(req,res) => {
   User.find({},(err,result) => {
-    if(err) throw err;
-    res.send(result)
+      if(err) throw err;
+      res.send(result)
+  })
 })
-});
 
-router.post("/register", (req, res) => {
-  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
-  User.create(
-    {
-      username: req.body.username,
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword,
-      phone: req.body.phone,
-      role: req.body.role,
-      qualification: req.body.qualification,
-    },
-    function (err, user) {
-      if (err) {
-        res.status(500).send(err);
-      }
-      res.json(user);
-    }
-  );
-});
+router.post('/register',(req,res) => {
+  var hashpassword = bcrypt.hashSync(req.body.password,8);
+  User.create({
+      name:req.body.name,
+      email:req.body.email,
+      password:hashpassword,
+      phone:req.body.phone,
+      role:req.body.role?req.body.role:'User'
+  },(err,data) => {
+      if(err) return res.status(500).send('Error')
+      res.status(200).send('Register success')
+  })
+})
 
 router.post('/login',(req,res) => {
-  User.findOne({username: req.body.username},(err,user) => {
+  User.findOne({email:req.body.email},(err,user) => {
       if(err) return res.status(500).send("Error")
       if(!user) return res.status(500).send({auth:false,token:'No user Found'})
       else{
@@ -50,25 +43,15 @@ router.post('/login',(req,res) => {
   })
 })
 
-router.get("/userinfo", function (req, res) {
-  var token = req.headers["x-access-token"];
-  if (!token) {
-    res.status(401).send({
-      auth: false,
-      message: "No token provided.",
-    });
-  } else {
-    jwt.verify(token, config.secret, function (err, user) {
-      if (err) {
-        res.status(500).send({
-          auth: false,
-          message: "Token authentication failed.",
-        });
-      } else {
-        res.status(200).send(user);
-      }
-    });
-  }
-});
+router.get('/userInfo',(req,res) => {
+  var token = req.headers['x-access-token'];
+  if(!token) return res.status(500).send({auth:false,token:'No Token Provided'})
+  jwt.verify(token, config.secret, (err,user) => {
+      if(err) return res.status(500).send({auth:false,token:'Invalid Token'})
+      User.findById(user.id,(err,result) => {
+          res.send(result)
+      })
+  })
+})
 
 module.exports = router;
